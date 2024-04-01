f183c803a72dcc31854fea7fcf8fc1de8f84267d
vando <vandorramos@gmail.com>
2024-04-01 13:31:58 -0300

    Ajusting template

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,11 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+(() => {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
